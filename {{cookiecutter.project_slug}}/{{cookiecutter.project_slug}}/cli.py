091c107e3ec7f73b0e043bc4c80eb7e26701bb11
herve beraud <herve.beraud@openmailbox.org>
2016-08-18 18:11:42 +0200

    hello world example

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -5,9 +5,9 @@ import click
 @click.command()
 def main(args=None):
     """Console script for {{cookiecutter.project_slug}}"""
-    click.echo("Replace this message by putting your code into "
-                "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo("#"*50)
+    click.echo(click.style("{{cookiecutter.project_name}}", fg='green', bold=True))
+    click.echo("#"*50)
 
 
 if __name__ == "__main__":
