c846d622f0a8c2628413aab6e8a3ce5fd49e69f0
herve beraud <herve.beraud@openmailbox.org>
2016-08-19 00:23:27 +0200

    fix git init

--- {{cookiecutter.project_slug}}/gitinit.sh
+++ {{cookiecutter.project_slug}}/gitinit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git init
 git add .
+git commit -am "first commit"
 git remote add \
     origin https://github.com/microservice-projects/{{ cookiecutter.project_slug | replace("_", "-") }}
 git push origin master
