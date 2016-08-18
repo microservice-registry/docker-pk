be6f6ee82bb198caa5f330f450b5a63979f4119a
herve beraud <herve.beraud@openmailbox.org>
2016-08-18 17:27:48 +0200

    fix requirements

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 with open('requirements.txt') as requirements_file:
-    required = [package for package in requirements_file.read() if not package.startwiths("#")]
+    required = [package for package in requirements_file.read() if not package.startswith("#")]
 
 requirements = [
     {%- if cookiecutter.command_line_interface|lower == 'click' %}
